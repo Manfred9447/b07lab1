c757a9dff85c5fc840fa02b40771e879ba221492
Manfred <test@gmail.com>
2023-09-28 13:13:02 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,40 @@
+import java.io.File;
+
 public class Driver {
 	public static void main(String[] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double[] c1 = { 6, 0, 0, 5 };
-		Polynomial p1 = new Polynomial(c1);
-		double[] c2 = { 0, -2, 0, 0, -9 };
-		Polynomial p2 = new Polynomial(c2);
+		double[] c1 = { -6, -2, 4, 5 };
+		int[] e1 = { 0, 1, 2, 3 };
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = { 7, -2, -9 };
+		int[] e2 = { 0, 1, 3 };
+		Polynomial p2 = new Polynomial(c2, e2);
 		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if (s.hasRoot(1))
-			System.out.println("1 is a root of s");
-		else
-			System.out.println("1 is not a root of s");
+		Polynomial p3 = p1.multiply(p2);
+
+		p1.saveToFile("p1.txt");
+		Polynomial p11 = new Polynomial(new File("p1.txt"));
+		p11.saveToFile("p11.txt");
+		p2.saveToFile("p2.txt");
+		s.saveToFile("s.txt");
+		p3.saveToFile("p3.txt");
+
+		Polynomial c = new Polynomial(new double[] { 0 }, new int[] { 0 });
+		c.saveToFile("c.txt");
+		Polynomial cc = new Polynomial(new File("c.txt"));
+		cc.saveToFile("cc.txt");
+		
+				
+		System.out.println("p1(2) = " + p1.evaluate(2));
+		System.out.println("p2(-1) = " + p2.evaluate(-1));
+		if (p1.hasRoot(1)) {
+			System.out.println("1 is a root of p1");
+		} else {
+			System.out.println("1 is not a root of p1");
+		}
+		if (p2.hasRoot(-1)) {
+			System.out.println("-1 is a root of p2");
+		} else {
+			System.out.println("-1 is not a root of p2");
+		}
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,54 +1,155 @@
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Polynomial {
 	private double[] coefficients;
+	private int[] exponents;
 
 	public Polynomial() {
 		coefficients = new double[0];
+		exponents = new int[0];
 	}
 
-	public Polynomial(double[] coefficients) {
+	public Polynomial(double[] coefficients, int[] exponents) {
 		this.coefficients = coefficients;
+		this.exponents = exponents;
 	}
 
-	public double[] getCoefficients() {
-		return coefficients;
+	public Polynomial(File file) {
+		Scanner scanner;
+		try {
+			scanner = new Scanner(file);
+			String s = scanner.nextLine();
+			Map<Integer, Double> m = new TreeMap<>();
+			String coe = "";
+			String exp = "";
+			boolean isCoe = true;
+			for (int i = 0; i < s.length(); i++) {
+				if (s.charAt(i) == '-' || s.charAt(i) == '+') {
+					if (coe.length() > 0) {
+						if (exp.length() == 0) {
+							exp = "0";
+						}
+						m.put(Integer.parseInt(exp), Double.parseDouble(coe));
+					}
+					coe = "";
+					exp = "";
+					isCoe = true;
+					coe += s.charAt(i);
+				} else if (s.charAt(i) == 'x') {
+					isCoe = false;
+				} else {
+					if (isCoe) {
+						coe += s.charAt(i);
+					} else {
+						exp += s.charAt(i);
+					}
+				}
+			}
+			if (coe.length() > 0) {
+				if (exp.length() == 0) {
+					exp = "0";
+				}
+				m.put(Integer.parseInt(exp), Double.parseDouble(coe));
+			}
+
+			coefficients = new double[m.size()];
+			exponents = new int[m.size()];
+			int index = 0;
+			for (int e : m.keySet()) {
+				exponents[index] = e;
+				coefficients[index] = m.get(e);
+				index++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Polynomial add(Polynomial p) {
-		double[] c1 = getCoefficients();
-		double[] c2 = p.getCoefficients();
-		if (c1.length > c2.length) {
-			double[] c = new double[c1.length];
-			for (int i = 0; i < c1.length; i++) {
-				if (i < c2.length) {
-					c[i] = c1[i] + c2[i];
-				} else {
-					c[i] = c1[i];
-				}
-			}
-			return new Polynomial(c);
+		Map<Integer, Double> m = new TreeMap<>();
+		int length;
+		if (coefficients.length < p.coefficients.length) {
+			length = p.coefficients.length;
 		} else {
-			double[] c = new double[c2.length];
-			for (int i = 0; i < c2.length; i++) {
-				if (i < c1.length) {
-					c[i] = c1[i] + c2[i];
-				} else {
-					c[i] = c2[i];
-				}
-			}
-			return new Polynomial(c);
+			length = coefficients.length;
 		}
+
+		for (int i = 0; i < length; i++) {
+			if (i < exponents.length) {
+				m.put(exponents[i], m.getOrDefault(exponents[i], 0.0) + coefficients[i]);
+			}
+			if (i < p.exponents.length) {
+				m.put(p.exponents[i], m.getOrDefault(p.exponents[i], 0.0) + p.coefficients[i]);
+			}
+		}
+		double[] coe = new double[m.size()];
+		int[] exp = new int[m.size()];
+		int index = 0;
+		for (int e : m.keySet()) {
+			exp[index] = e;
+			coe[index] = m.get(e);
+			index++;
+		}
+		return new Polynomial(coe, exp);
 	}
 
 	public double evaluate(double x) {
 		double res = 0.0;
-		double[] c = getCoefficients();
-		for (int i = 0; i < c.length; i++) {
-			res += c[i] * Math.pow(x, i);
+		for (int i = 0; i < coefficients.length; i++) {
+			res += coefficients[i] * Math.pow(x, exponents[i]);
 		}
 		return res;
 	}
 
 	public boolean hasRoot(double x) {
-		return Math.abs(x) < 1e6;
+		return Math.abs(evaluate(x)) < 1e-6;
 	}
+
+	public Polynomial multiply(Polynomial p) {
+		Map<Integer, Double> m = new TreeMap<>();
+		for (int i = 0; i < exponents.length; i++) {
+			for (int j = 0; j < p.exponents.length; j++) {
+				m.put(exponents[i] + p.exponents[j],
+						m.getOrDefault(exponents[i] + p.exponents[j], 0.0) + coefficients[i] * p.coefficients[j]);
+			}
+		}
+		double[] coe = new double[m.size()];
+		int[] exp = new int[m.size()];
+		int index = 0;
+		for (int e : m.keySet()) {
+			exp[index] = e;
+			coe[index] = m.get(e);
+			index++;
+		}
+		return new Polynomial(coe, exp);
+	}
+
+	public void saveToFile(String path) {
+		String out = "";
+		for (int i = 0; i < coefficients.length; i++) {
+			if (exponents[i] == 0) {
+				out += coefficients[i];
+			} else if (coefficients[i] > 0) {
+				out += "+" + coefficients[i] + "x" + exponents[i];
+			} else {
+				out += coefficients[i] + "x" + exponents[i];
+			}
+		}
+		System.out.println(out);
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+			bw.append(out + "\n");
+			bw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// read from file
 }
